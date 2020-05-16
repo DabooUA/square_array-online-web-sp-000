c8e5978e64d4691157e11a80e823995e54c78922
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-16 01:33:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -5,3 +5,9 @@ def square_array(array)
   end
   return new_numbers
 end
+
+def square_array(array)
+  new_numbers = []
+  array.collect << new_numbers {|num| ** 2} |num|
+    new_numbers << (num ** 2)
+end
